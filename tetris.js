929a2c90da2686a65a0ee8a289468e0accd129c6
returntrip444 <danp777991@gmail.com>
2020-06-25 16:36:32 -0700

    second stage of code

--- tetris.js
+++ tetris.js
@@ -3,12 +3,42 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function createMatrix(numberOfItems) {
+
+
+const matrix =  []
+
+for (let i = 0; i < 3; i++) {
+    const numbers = []
+    matrix.push(numbers)
+    for (let j = 0; j < numberOfItems; j++) {
+        let randomNumber = getRandomNumber()
+     numbers.push(randomNumber)
+   
+    }  
+}
+   console.log(matrix)
+   return matrix
+}
+
+function getRandomNumber() {
+    const randomNumber = Math.random()
+    if (randomNumber < 0.5) {
+        return 0;
+    }
+    else if (randomNumber > 0.5) {
+        return 1;
+    }
+}
+// createMatrix(4, 1)
 
 
 const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
     [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
 
 ];
 
@@ -23,21 +53,20 @@ matrix.forEach((row, y) => {
     row.forEach((value, x) => {
         if (value !== 0) {
             context.fillStyle = 'blue';
-            context.fillRect(x + offset.x,
-                
-                         y + offset.y,
-                         
-                         1, 1);
+            context.fillRect(x + offset.x, y + offset.y, 1, 1);
         }
     });
 });
 
 }
 
-function update() {
-    draw(),
-    requestAnimationFrame(update)
+function update(timestamp) {
+    draw()
+    console.log(timestamp)
 }
+
+
+requestAnimationFrame(update)
 
 const player = {
     pos: {x: 5, y:5},
