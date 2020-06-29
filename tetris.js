6603c1d870453e29a4bbbcb148bf979f08b009ac
returntrip444 <danp777991@gmail.com>
2020-06-29 14:36:28 -0700

    collision

--- tetris.js
+++ tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
+
 const matrix = [
 
     [0, 0, 0],
@@ -77,10 +78,42 @@ const matrix8 = [
     
 ];
 
-const pieces = [matrix, matrix1, matrix2, matrix3, matrix4, matrix5, matrix6, matrix7, matrix8]
-const randomPieces =  pieces[Math.floor(Math.random() * pieces.length)]
-   console.log(randomPieces)
-   console.log(Math.floor(Math.random() * pieces.length + 1))
+function generatePieces(type) {
+    switch(type) {
+        case "0":
+            return matrix;
+        case "1":
+            return matrix1;
+        case "2":
+            return matrix2;
+        case "3":
+            return matrix3;
+        case "4":
+            return matrix4;
+        case "5":
+            return matrix5;
+        case "6":
+            return matrix6;
+        case "7":
+            return matrix7;
+        case "8":
+            return matrix8;
+        default:
+            return;
+    }
+}
+
+function playerReset() {
+    const pieces = "012345678"
+    player.matrix = generatePieces(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length /2 | 0) - (player.matrix[0].length / 2 | 0);
+    if(collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+    }
+}
+
+
 
 const colors = [
     
@@ -93,16 +126,18 @@ const colors = [
     'red'
   ];
 
-  const randomColors = colors[Math.floor(Math.random() * colors.length)]
-  console.log(randomColors)
+//   const randomColors = colors[Math.floor(Math.random() * colors.length)]
+//   console.log(randomColors)
 
 
 function collide(arena, player) {
-    const[m, o] = [player.matrix, player.pos];
+    // const[m, o] = [player.matrix, player.pos];
+    const m = player.matrix;
+    const o = player.pos;
      for (let y = 0; y < m.length; ++y) {
          for (let x =0; x < m[y].length; ++x) {
-             if ((m[y][x] !== 0 &&
-                arena[y + o.y] &&
+             if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
                 arena[y + o.y] [x + o.x]) !== 0) {
 
                     return true;
@@ -137,7 +172,7 @@ function drawMatrix(matrix, offset) {
 matrix.forEach((row, y) => {
     row.forEach((value, x) => {
         if (value !== 0) {
-            context.fillStyle = randomColors;
+            context.fillStyle = colors[value];
             context.fillRect(x + offset.x, y + offset.y, 1, 1);
         }
     });
@@ -149,7 +184,8 @@ function merge( arena, player) {
     player.matrix.forEach((row, y) => {
      row.forEach((value, x) => {
          if (value !== 0) {
-             arena[y + player.pos.y][x +player.pos.x] = value;
+             return arena[y + player.pos.y][x + player.pos.x] = value;
+            //  console.log(value)
 
          }
      });
@@ -164,17 +200,20 @@ function merge( arena, player) {
 }
 
  function playerDrop() {
-    player.pos.y = 15.5;
+    // player.pos.y = 15.5;
+    player.pos.y++;
      if (collide(arena, player)) {
          player.pos.y--;
          merge(arena, player);
-         player.pos.y = 0;
+        //  player.pos.y = 0;
+        playerReset();
 
      }
     dropCounter = 0;
  }
  
 function playerMove(dir) {
+    console.log(player.pos.x += dir);
     player.pos.x += dir;
     if (collide (arena, player)) {
         player.pos.x -= dir;
@@ -186,15 +225,17 @@ let dropCounter = 0;
 let dropInterval = 1000;
 
 let lastTime = 0;
+
 function update(time = 0) {
     const deltaTime = time- lastTime;
-    lastTime = time
-
+    
     dropCounter += deltaTime;
-     if (dropCounter > dropInterval) {
-         player.pos.y++;
-         dropCounter = 0;
-     }
+    if (dropCounter > dropInterval) {
+        //  player.pos.y++;
+        //  dropCounter = 0;
+        playerDrop();
+    }
+    lastTime = time
     draw()
     requestAnimationFrame(update)
     
@@ -207,26 +248,23 @@ const arena = createMatrix(12, 20)
 
 const player = {
     pos: {x: 0, y: 0},
-    matrix: randomPieces
+    matrix: null,
 }
 
 document.addEventListener('keydown', event => {
+    console.log(event.keyCode)
    if (event.keyCode === 37) {
-    player.pos.x --;
        playerMove(-1);
-       
    }
     else if (event.keyCode === 39) {
-        player.pos.x ++;
         playerMove(1);
-        
     }
     else if (event.keyCode === 40) {
-        
         playerDrop()
     }
 
 
 });
 
-update()
+playerReset();
+update();
